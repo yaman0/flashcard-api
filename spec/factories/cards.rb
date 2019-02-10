2d7470eba36ff4ccde7674d4d57de521625a703f
yamano <yaman0@protonmail.com>
2019-02-10 20:41:33 +0100

    test(): fix rubocop warning with static factories' element

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :card do
     front { Faker::Lorem.word }
     back { Faker::Lorem.word }
-    collection_id nil
+    collection_id { nil }
   end
 end
--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :collection do
     title { Faker::StarWars.character }
-    favorite false
+    favorite { false }
     created_by { Faker::Number.number(10) }
   end
 end
